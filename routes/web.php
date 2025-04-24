a2a84c6f684aadc2faec527b1a0ab301a794e395
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2025-04-24 19:19:21 +0700

    pemindahan file agar rapih

--- routes/web.php
+++ routes/web.php
@@ -9,31 +9,52 @@ Route::get('/', function () {
 
 // Onboarding Pages
 Route::get('/get-started', function () {
-    return view('get-started');
+    return view('Auth.get-started');
 })->name('get.started');
 
 // Authentication
 Route::get('/sign-in', function () {
-    return view('sign-in');
+    return view('Auth.sign-in');
 })->name('sign.in');
- 
+
 Route::get('/sign-in-employee', function () {
-    return view('sign-in-employee');
+    return view('Auth.sign-in-employee');
 })->name('sign.in.employee');
 
 Route::get('/forgot-password', function () {
-    return view('forgot-password');
+    return view('Auth.forgot-password');
 })->name('forgot.password');
+
 Route::get('/check-your-email', function () {
-    return view('check-your-email');
+    return view('Auth.check-your-email');
 })->name('check.your.email');
+
 Route::get('/set-new-password', function () {
-    return view('set-new-password');
+    return view('Auth.set-new-password');
 })->name('set.new.password');
+
 Route::get('/link-expired', function () {
-    return view('link-expired');
+    return view('Auth.link-expired');
 })->name('link.expired');
 
+// Employee Database
 Route::get('/employee-database', function () {
-    return view('employee-database');
+    return view('Employee.employee-database');
 })->name('employee.database');
+
+// Checklock Absen Routes
+Route::get('/admin-checklock', function () {
+    return view('Checklock.admin_checklock');
+})->name('checklock.admin');
+
+Route::get('/admin-absensi', function () {
+    return view('Checklock.admin_absensi');
+})->name('checklock.admin.absensi');
+
+Route::get('/user-checklock', function () {
+    return view('Checklock.user_checklock');
+})->name('checklock.user');
+
+Route::get('/user-absensi', function () {
+    return view('Checklock.user_absensi');
+})->name('checklock.user.absensi');
