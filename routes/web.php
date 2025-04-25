cd9a495a43b4ddd53c9d9402deec24296282d460
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2025-04-25 10:07:21 +0700

    perbaikan admin dan route

--- routes/web.php
+++ routes/web.php
@@ -9,61 +9,61 @@ Route::get('/', function () {
 
 // Onboarding Pages
 Route::get('/get-started', function () {
-    return view('auth/get-started');
+    return view('auth.get-started');
 })->name('get.started');
 
 // Authentication
 Route::get('/sign-in', function () {
-    return view('auth/sign-in');
+    return view('auth.sign-in');
 })->name('sign.in');
- 
+
 Route::get('/sign-in-employee', function () {
-    return view('auth/sign-in-employee');
+    return view('auth.sign-in-employee');
 })->name('sign.in.employee');
 
 Route::get('/forgot-password', function () {
-    return view('auth/forgot-password');
+    return view('auth.forgot-password');
 })->name('forgot.password');
 
 Route::get('/check-your-email', function () {
-    return view('auth/check-your-email');
+    return view('auth.check-your-email');
 })->name('check.your.email');
 
 Route::get('/set-new-password', function () {
-    return view('auth/set-new-password');
+    return view('auth.set-new-password');
 })->name('set.new.password');
 
 Route::get('/auth/link-expired', function () {
-    return view('auth/link-expired');
+    return view('auth.link-expired');
 })->name('link.expired');
 
-//dashboard
-Route::get('/admin_dashboard', function () {
-    return view('admin_dashboard');
+// Dashboard
+Route::get('/admin-dashboard', function () {
+    return view('dasbord.admin_dashboard');
 })->name('admin_dashboard');
 
-//Employee
+// Employee
 Route::get('/employee-database', function () {
-    return view('employee/employee-database');
+    return view('employee.employee-database');
 })->name('employee.database');
 
-Route::get('new-emplpoyee',function(){
-    return view('employee/new-employee');
+Route::get('/new-employee', function () {
+    return view('employee.new-employee');
 })->name('new.employee');
 
-//checklock
-Route::get('/user_checklock', function () {
-    return view('checklock/user_checklock');
+// Checklock
+Route::get('/user-checklock', function () {
+    return view('checklock.user_checklock');
 })->name('user_checklock');
 
-Route::get('/user_absensi', function () {
-    return view('checklock/user_absensi');
-})->name('user_absensi');    
+Route::get('/user-absensi', function () {
+    return view('checklock.user_absensi');
+})->name('user_absensi');
 
-Route::get('/admin_checklock', function () {
-    return view('checklock/admin_checklock');
+Route::get('/admin-checklock', function () {
+    return view('checklock.admin_checklock');
 })->name('admin_checklock');
 
-Route::get('/admin_absensi', function () {
-    return view('checklock/admin_absensi');
+Route::get('/admin-absensi', function () {
+    return view('checklock.admin_absensi');
 })->name('admin_absensi');
